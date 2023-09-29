c310dbaac0225010bd44cde1afd64334bbfbeb11
EshimovShaxzod <126951571+EshimovShaxzod@users.noreply.github.com>
2023-09-29 10:00:49 +0500

    adam JOB figma

---
+++ js/app.js
@@ -0,0 +1,10 @@
+AOS.init();
+
+let navList = document.querySelector('.nav_list');
+let navBurgerMenu = document.querySelector('.bx bx-menu');
+navXMenu = document.querySelector('.bx bx-x');
+
+
+
+
+
